bdf01b8f42b816cd0bf76b8c5a4914a41fdc29cd
Romin Kevadiya <rominkevadiya@gmail.com>
2026-03-27 13:22:45 +0530

    Delete lec_8(3).js

--- lec_8(3).js
+++
@@ -1,8 +0,0 @@
-var http = require('http')
-
-http.createServer(function(req, res) {
-    res.writeHead(200, {'content-type' : 'text/html'})
-    res.write(req.url + "<br>")
-    res.end("url-fetched")
-
-}).listen(3120)
