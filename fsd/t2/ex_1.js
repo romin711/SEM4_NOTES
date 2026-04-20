b2bee909a484691f2af807f50ce2b533207a7161
Romin Kevadiya <rominkevadiya@gmail.com>
2026-04-20 10:35:39 +0530

    Delete fsd/t2 directory

--- fsd/t2/ex_1.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var app = express()
-
-app.get('/' , function(req , res) {
-    res.set('content-type' , 'text/html')
-    // res.type('text/plain')
-    // res.setHeader('content-type' , 'text/plain')
-    res.send("<h1>hello</h1>")
-}).listen(3000)
--- fsd/t2/ex_4.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var app = express()
-
-app.get('/calendar/:day/event/:ename' , (req , res) => {
-    res.send(req.params)
-}).listen(3000 , () => {
-    console.log("server started")
-})
